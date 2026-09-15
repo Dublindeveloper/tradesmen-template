a404a60e0a54540b4043c23c4e9b7e33de4d087f
Jinish Rajan <connectjinish@gmail.com>
2026-09-15 15:47:56 +0100

    docs: add live Cloudflare deployment URLs and market pricing updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: 'https://dublinelectrician.ie',
   integrations: [sitemap()],
   compressHTML: true,
+
   build: {
     inlineStylesheets: 'always',
   },
+
+  adapter: cloudflare()
 });
